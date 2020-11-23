673656005051bc14c0a2bf1d09477a787b1fdbcf
Andrea <grassiandrea95@gmail.com>
2020-11-23 18:24:33 +0100

    chat dinamiche

--- js/script.js
+++ js/script.js
@@ -3,22 +3,105 @@ var app= new Vue({
   data:{
     contattoAttivo:0,
     contact:[
+
+      // utente1
       {
         img:'img/avatar_10.jpg',
         name:'Alessia',
-        lastMsg:'ciao',
+        lastMsg:'',
         clock:'20-11-2020 10:10:00',
+        chatIndex:0,
         chat:[
-
+          {
+            msg:'ciao come stai',
+            users:'me'
+          },
+          {
+            msg:'tutto bene',
+            users:'you'
+          },
+          {
+            msg:'ok',
+            users:'me'
+          }
         ]
-
       },
+
+      // utente2
       {
         img:'img/avatar_2.jpg',
         name:'Marco',
-        lastMsg:'ok',
+        lastMsg:'',
+        clock:'22-11-2020 10:20:00',
+        chat:[
+          {
+            msg:'ciao ciao',
+            users:'me'
+          },
+          {
+            msg:'come va',
+            users:'me'
+          },
+          {
+            msg:'bene',
+            users:'you'
+          }
+        ]
+      },
+
+      // utente3
+      {
+        img:'img/avatar_3.jpg',
+        name:'Luca',
+        lastMsg:'',
+        clock:'23-11-2020 16:20:00',
+        chat:[
+          {
+            msg:'dsjkjdkas',
+            users:'me'
+          },
+          {
+            msg:'asdaksjd kasjdkasd djkj dsdjsajdasj',
+            users:'me'
+          },
+          {
+            msg:'bene',
+            users:'you'
+          },
+          {
+            msg:'asdaksjd kasjdkasd djkj dsdjsajdasj',
+            users:'me'
+          }
+        ]
+      },
+
+      // utente4
+      {
+        img:'img/avatar_4.jpg',
+        name:'Marco',
+        lastMsg:'',
         clock:'25-11-2020 10:20:00',
-      }
+        chat:[
+          {
+            msg:'ciao ciao',
+            users:'me'
+          },
+          {
+            msg:'come va',
+            users:'me'
+          },
+          {
+            msg:'bene',
+            users:'you'
+          }
+        ]
+      },
+
+
+
+
+
+
     ]
   },
   methods:{
