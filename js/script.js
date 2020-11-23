5bb226647170096cd171c844f7716de30be549bd
Andrea <grassiandrea95@gmail.com>
2020-11-23 16:42:33 +0100

    aggiunta classe active

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 var app= new Vue({
   el:'#app',
   data:{
-    
+    contattoAttivo:0,
+    contact:[
+      {
+        img:'img/avatar_10.jpg',
+        name:'Alessia',
+        lastMsg:'ciao',
+        clock:'20-11-2020 10:10:00',
+        chat:[
+
+        ]
+
+      },
+      {
+        img:'img/avatar_2.jpg',
+        name:'Marco',
+        lastMsg:'ok',
+        clock:'25-11-2020 10:20:00',
+      }
+    ]
   },
   methods:{
-
+    chatAttiva(i){
+      this.contattoAttivo=i;
+    }
   }
 });
