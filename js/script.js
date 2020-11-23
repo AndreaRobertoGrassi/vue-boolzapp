dd012708785c8b3a63d2b6a451585c8b894ee08b
Andrea <grassiandrea95@gmail.com>
2020-11-23 20:20:00 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
 var app= new Vue({
   el:'#app',
   data:{
+    // indice per scorrere l'array contact
     contattoAttivo:0,
+    // contatti
     contact:[
 
       // utente1
       {
         img:'img/avatar_10.jpg',
         name:'Alessia',
-        lastMsg:'',
-        clock:'20-11-2020 10:10:00',
         chatIndex:0,
         chat:[
           {
             msg:'ciao come stai',
-            users:'me'
+            users:'me',
+            lastSeen:'22-11-2020 17:57:00'
           },
           {
             msg:'tutto bene',
-            users:'you'
+            users:'you',
+            lastSeen:'22-11-2020 18:03:00'
           },
           {
             msg:'ok',
-            users:'me'
+            users:'me',
+            lastSeen:'22-11-2020 18:10:00'
           }
         ]
       },
@@ -31,20 +34,21 @@ var app= new Vue({
       {
         img:'img/avatar_2.jpg',
         name:'Marco',
-        lastMsg:'',
-        clock:'22-11-2020 10:20:00',
         chat:[
           {
             msg:'ciao ciao',
-            users:'me'
+            users:'me',
+            lastSeen:'22-11-2020 10:12:00'
           },
           {
             msg:'come va',
-            users:'me'
+            users:'me',
+            lastSeen:'22-11-2020 10:14:00'
           },
           {
             msg:'bene',
-            users:'you'
+            users:'you',
+            lastSeen:'22-11-2020 10:20:00'
           }
         ]
       },
@@ -53,24 +57,26 @@ var app= new Vue({
       {
         img:'img/avatar_3.jpg',
         name:'Luca',
-        lastMsg:'',
-        clock:'23-11-2020 16:20:00',
         chat:[
           {
             msg:'dsjkjdkas',
-            users:'me'
+            users:'me',
+            lastSeen:'23-11-2020 16:18:00'
           },
           {
             msg:'asdaksjd kasjdkasd djkj dsdjsajdasj',
-            users:'me'
+            users:'me',
+            lastSeen:'23-11-2020 16:19:00'
           },
           {
             msg:'bene',
-            users:'you'
+            users:'you',
+            lastSeen:'23-11-2020 16:19:00'
           },
           {
             msg:'asdaksjd kasjdkasd djkj dsdjsajdasj',
-            users:'me'
+            users:'me',
+            lastSeen:'23-11-2020 16:20:00'
           }
         ]
       },
@@ -79,32 +85,29 @@ var app= new Vue({
       {
         img:'img/avatar_4.jpg',
         name:'Marco',
-        lastMsg:'',
-        clock:'25-11-2020 10:20:00',
         chat:[
           {
             msg:'ciao ciao',
-            users:'me'
+            users:'me',
+            lastSeen:'25-11-2020 10:18:00'
           },
           {
             msg:'come va',
-            users:'me'
+            users:'me',
+            lastSeen:'25-11-2020 10:18:00'
           },
           {
-            msg:'bene',
-            users:'you'
+            msg:'bene jdhfjhsd dfhjdhfjf sdhfjsdf',
+            users:'you',
+            lastSeen:'25-11-2020 10:20:00'
           }
         ]
-      },
-
-
-
-
-
+      }
 
     ]
   },
   methods:{
+    // funzione per selezionare la chat cliccata
     chatAttiva(i){
       this.contattoAttivo=i;
     }
